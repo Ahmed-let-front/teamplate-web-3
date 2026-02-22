efec1a1d67ef7e9adf3a5c2776b0ab00c7a555c9
Ahmed <letcosdgp@gmail.com>
2026-02-22 07:07:41 +0200

    change performance

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
-  plugins: [tailwindcss()],
   base: "/teamplate-web-3/",
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
