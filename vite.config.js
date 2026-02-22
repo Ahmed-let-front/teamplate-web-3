16a64e5f1757cedb5c4517a836b2c6d1554b1a4f
Ahmed <letcosdgp@gmail.com>
2026-02-22 07:14:26 +0200

    change performance2

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: "/teamplate-web-3/",
@@ -6,12 +6,15 @@ export default defineConfig({
     chunkSizeWarningLimit: 2000,
     rollupOptions: {
       output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js',
         manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor";
+          if (id.includes('node_modules')) {
+            return 'vendor';
           }
-        },
-      },
-    },
-  },
-});
+        }
+      }
+    }
+  }
+})
