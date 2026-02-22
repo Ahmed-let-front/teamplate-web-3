cfdb544a3b3e9e8095d0df161d8b99d4ad18efd4
Ahmed <letcosdgp@gmail.com>
2026-02-22 02:31:40 +0200

    change

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [tailwindcss()],
-  base: "/template-web-3/",
+  base: "/teamplate-web-3/",
 });
